4664c48f5e81725b45ad09e38a6f6bdb1fd2e3f2
RyanXin <xinzishu@gmail.com>
2024-01-29 23:56:45 -0800

    update remove sql

--- index.py
+++ index.py
@@ -17,20 +17,20 @@ import numpy as np
 
 #查询数据库
 # connection = create_connection("localhost", "root", "password", "database_name")
-def create_connection(host_name, user_name, user_password, db_name):
-    connection = None
-    try:
-        connection = mysql.connector.connect(
-            host=host_name,
-            user=user_name,
-            passwd=user_password,
-            database=db_name
-        )
-        print("Connection to MySQL DB successful")
-    except Exception as e:
-        print(f"The error '{e}' occurred")
-    return connection
-
+# def create_connection(host_name, user_name, user_password, db_name):
+#     connection = None
+#     try:
+#         connection = mysql.connector.connect(
+#             host=host_name,
+#             user=user_name,
+#             passwd=user_password,
+#             database=db_name
+#         )
+#         print("Connection to MySQL DB successful")
+#     except Exception as e:
+#         print(f"The error '{e}' occurred")
+#     return connection
+ 
 def process_data(df):
     # 数据清洗和转换
     df['Price'] = df['Price'].replace('[\$,]', '', regex=True).astype(float)
